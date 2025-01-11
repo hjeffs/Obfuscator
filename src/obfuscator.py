528d0d4fab4077c6ece46ae9f617661576545dc5
Harry Jeffs <hjeffs30@gmail.com>
2025-01-11 15:25:40 +0000

    Obfuscating the correct fields with this method

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -1,19 +1,32 @@
 import csv
-import sys
-
-# print(row, flush=True)
 
 def obfuscate(file_to_obfuscate, pii_fields):
   try:
-    with open('./files/testdata_10students.csv') as test:
+    with open(file_to_obfuscate) as test:
         print ("Starting obfuscation...") # Debug statement
+        print ("Fields to be Obfuscated:", pii_fields)
         reader = csv.reader(test, delimiter=',')
+        obfuscated_data = []
+        indexes_to_obfuscate =[]
+
         for row in reader:
-            print(row, flush=True)
+          for column in pii_fields:
+              if column in row:
+                print(column, '<<< column')
+                print(row.index(column), '<<< index')
+                indexes_to_obfuscate.append(row.index(column))
+          print(row)
+          
+          for index in indexes_to_obfuscate:
+              row[index] = '***' # Obfuscated value from brief
+          obfuscated_data.append(row)
+        
+        print(obfuscated_data)
+
   except FileNotFoundError:
-    print("Error: CSV file not found.")
+    print("Error: file not found.")
   except Exception as e:
     print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-  obfuscate('./files/testdata_10students.csv', 'name')
+  obfuscate('./files/testdata_10students.csv', ['name', 'email_address'])
