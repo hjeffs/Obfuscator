5125bc31ab45b6c66f3d0677e479ce2891b2236f
Harry Jeffs <hjeffs30@gmail.com>
2025-01-11 14:11:59 +0000

    Reading test data csv to see output of file

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -1,4 +1,19 @@
-def obfuscate(file_to_obfuscate, pii_fields):
-  return file_to_obfuscate + pii_fields
+import csv
+import sys
 
-# I am testing git push
+# print(row, flush=True)
+
+def obfuscate(file_to_obfuscate, pii_fields):
+  try:
+    with open('./files/testdata_10students.csv') as test:
+        print ("Starting obfuscation...") # Debug statement
+        reader = csv.reader(test, delimiter=',')
+        for row in reader:
+            print(row, flush=True)
+  except FileNotFoundError:
+    print("Error: CSV file not found.")
+  except Exception as e:
+    print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+  obfuscate('./files/testdata_10students.csv', 'name')
