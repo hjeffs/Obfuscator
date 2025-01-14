d7584cab06a707ca14d54b3c0d0fcdd9f774753b
Harry Jeffs <hjeffs30@gmail.com>
2025-01-14 14:05:58 +0000

    Created a single student test file and an output file to write to. Added writing method and changed indentation of append to stop duplicates

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -1,6 +1,6 @@
 import csv
 
-def obfuscate(file_to_obfuscate, pii_fields):
+def obfuscate(file_to_obfuscate, pii_fields, obfuscated_file):
   try:
     with open(file_to_obfuscate) as input:
         reader = csv.DictReader(input)
@@ -16,10 +16,15 @@ def obfuscate(file_to_obfuscate, pii_fields):
               if field in row:
                 # print(field, '<<< field')
                 row[field] = '***' # Obfuscated value from brief
-                obfuscated_data.append(row)
+          obfuscated_data.append(row)
           # print(row)
         
         print(obfuscated_data)
+      
+    with open(obfuscated_file, mode='w', newline='') as output:
+        writer = csv.DictWriter(output, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(obfuscated_data)
 
   except FileNotFoundError:
     print("Error: file not found.")
@@ -27,4 +32,4 @@ def obfuscate(file_to_obfuscate, pii_fields):
     print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-  obfuscate('./files/testdata_10students.csv', ['name', 'email_address'])
+  obfuscate('./files/testdata_1student.csv', ['name', 'email_address', 'cohort'], './files/obfuscated_file.csv')
