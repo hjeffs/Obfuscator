bb9a6842491ba0240ea2888ec53abc34b403cdb6
Harry Jeffs <hjeffs30@gmail.com>
2025-01-14 20:46:22 +0000

    Reorganisation of tests to leave obfuscated_file.csv in obfuscated state when tests finish

--- tests/test_obfuscator.py
+++ tests/test_obfuscator.py
@@ -2,23 +2,6 @@ import csv
 import pytest
 from src.obfuscator import obfuscate
 
-def test_obfuscate():
-    # Arrange
-    input_file = './files/testdata_10students.csv'
-    pii_fields = ['name', 'email_address']
-    output_file = './files/obfuscated_file.csv'
-
-    # Act
-    obfuscate(input_file, pii_fields, output_file)
-
-    # Assert
-    with open(output_file, mode='r') as test:
-        reader = csv.DictReader(test)
-        for row in reader:
-            assert row['name'] == '***', "Name is obfuscated"
-            assert row['email_address'] == '***', "Email is obfuscated"
-            assert row['student_id'] != '***', "Student ID is not obfuscated"
-
 def test_obfuscate_no_file():
     # Arrange
     input_file = ''
@@ -102,3 +85,20 @@ def test_obfuscate_bad_field():
         for row in reader: 
             assert row['name'] != '***', "Name not obfuscated"
             assert row['email_address'] != '***', "Email not obfuscated"
+
+def test_obfuscate():
+    # Arrange
+    input_file = './files/testdata_10students.csv'
+    pii_fields = ['name', 'email_address']
+    output_file = './files/obfuscated_file.csv'
+
+    # Act
+    obfuscate(input_file, pii_fields, output_file)
+
+    # Assert
+    with open(output_file, mode='r') as test:
+        reader = csv.DictReader(test)
+        for row in reader:
+            assert row['name'] == '***', "Name is obfuscated"
+            assert row['email_address'] == '***', "Email is obfuscated"
+            assert row['student_id'] != '***', "Student ID is not obfuscated"
