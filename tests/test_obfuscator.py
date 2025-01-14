1d8eb02eff14f841981e46ed066721585d3a17f3
Harry Jeffs <hjeffs30@gmail.com>
2025-01-14 15:43:31 +0000

    Building test suite and 1st test to make sure method is working as intended.

--- tests/test_obfuscator.py
+++ tests/test_obfuscator.py
@@ -1,4 +1,19 @@
+import csv
+import pytest
 from src.obfuscator import obfuscate
 
 def test_obfuscate():
-  assert obfuscate(1, 1) == 2
+    input_file = 'files\testdata_1student.csv'
+    pii_fields = ['name', 'email_address']
+    output_file = 'files\obfuscated_file.csv'
+
+    # Run obfuscation
+    obfuscate(input_file, pii_fields, output_file)
+
+    # Check the output
+    with open(output_file, mode='r') as test:
+        reader = csv.DictReader(test)
+        for row in reader:
+            assert row['name'] == '***', "Name is obfuscated"
+            assert row['email_address'] == '***', "Email is obfuscated"
+            assert row['student_id'] != '***', "Student ID is not obfuscated"
