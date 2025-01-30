7b864d7ed7a8f78fc01627eec29e699d6852e9ca
Harry Jeffs <hjeffs30@gmail.com>
2025-01-30 15:56:06 +0000

    Refactor file_check and obfuscate_csv to deal with file_content, StringIO and returns byte_stream

--- src/obfuscate_csv.py
+++ src/obfuscate_csv.py
@@ -1,30 +1,33 @@
 import pandas as pd
 import os
-from io import StringIO
+from io import BytesIO, StringIO
 from obfuscate_data import obfuscate_data
 
-def obfuscate_csv(file_to_obfuscate, pii_fields):
+def obfuscate_csv(file_content, pii_fields):
     try:
-        open('./files/obfuscated_file.csv', 'w').close() # empty file
-        file_name, file_extension = os.path.splitext(file_to_obfuscate)
-        print(f'File name: {file_name}', '<<< obs_csv')
-        print(f'File type: {file_extension}', '<<< obs_csv')
+        print("in OBFUSCATE")
+        #open('./files/obfuscated_file.csv', 'w').close() # empty file
+        #file_name, file_extension = os.path.splitext(file_to_obfuscate)
+        #print(f'File name: {file_name}', '<<< obs_csv')
+        #print(f'File type: {file_extension}', '<<< obs_csv')
 
         # Load csv into dataframe
-        # index_col=0 arg ignores first column 
-        # caused problem obfuscating student_id
-        dataframe = pd.read_csv(file_to_obfuscate)
+        temporary_file = StringIO(file_content) # create temp file from content
+        dataframe = pd.read_csv(temporary_file)
+        #dataframe = pd.read_csv(file_to_obfuscate)
+        
 
         # Filter fields for whitespace / empty strings
         pii_fields = [field for field in pii_fields if field.strip()]
 
         # fixes no fields adding in another column
         if not pii_fields:      
-            output_csv = StringIO()  
-            dataframe.to_csv(output_csv, index=False, index_label=False)  
+            byte_stream = BytesIO() 
+            dataframe.to_csv(byte_stream, index=False, index_label=False)  
             dataframe.to_csv('./files/obfuscated_file.csv', index=False, index_label=False)    
-            output_csv.seek(0)  
-            return output_csv
+            #output_csv.seek(0)  
+            byte_stream.seek(0)
+            return byte_stream
 
 
         # Obfuscate pii_fields
@@ -32,17 +35,21 @@ def obfuscate_csv(file_to_obfuscate, pii_fields):
         print(dataframe, '<<< CSV (OUTPUT)')
 
         # Save the obfuscated dataframe to a new csv file using StringIO
-        output_csv = StringIO()                         # create file object
-        dataframe.to_csv(output_csv, index=False, index_label=False)     # write output
+        #output_csv = StringIO()                         # create file object
+        byte_stream = BytesIO() 
+        dataframe.to_csv(byte_stream, index=False, index_label=False) 
+        #dataframe.to_csv(output_csv, index=False, index_label=False)     # write output
         # False for index and index_label to make the same csv shape     
-        dataframe.to_csv('./files/obfuscated_file.csv', index=False, index_label=False) 
-        output_csv.seek(0)                              # set cursor at index 0
+        dataframe.to_csv('./files/obfuscated_file.csv', index=False, index_label=False)
+        dataframe.to_csv('./files/s3_test.csv', index=False, index_label=False)  
+        #output_csv.seek(0)                              # set cursor at index 0
+        byte_stream.seek(0)
 
-        return output_csv
+        return byte_stream
     except FileNotFoundError:
-        raise FileNotFoundError("Error: Input file not found.")
+        raise FileNotFoundError("Error: Input file not found.", '<<< obs_csv error')
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'Error: {e}', '<<< obs_csv error')
         raise e
     
 if __name__ == "__main__":
