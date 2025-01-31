f0175838b3e947ed771fe93ef2a6aaa1b7b62013
Harry Jeffs <hjeffs30@gmail.com>
2025-01-31 14:04:50 +0000

    Creating __all__ in init and relative targeting

--- obfuscator/__init__.py
+++ obfuscator/__init__.py
@@ -1,2 +1,6 @@
-# Import functions for easier access
-from obfuscator import obfuscate, obfuscate_data, read_file, s3_parser, write_to_bytestream
+from .obfuscate import obfuscate, obfuscate_data
+from .read_file import read_file
+from .s3_parser import s3_parser
+from .write_to_bytestream import write_to_bytestream
+
+__all__ = ['obfuscate', 'obfuscate_data', 'read_file', 's3_parser', 'write_to_bytestream']
