634ff050e27f57345edd96bf8eb96505d70f2902
Harry Jeffs <hjeffs30@gmail.com>
2025-02-03 14:08:41 +0000

    Removing dataframe print prior to obfuscation

--- obfuscator/obfuscate_data.py
+++ obfuscator/obfuscate_data.py
@@ -1,5 +1,5 @@
 def obfuscate_data(dataframe, fields):
-    print(dataframe, '<<< DATA (INPUT) in obfuscate_data.py')
+    # print(dataframe, '<<< DATA (INPUT) in obfuscate_data.py')
     for field in fields:
         if field in dataframe.columns:
             dataframe[field] = '***' # Obfuscated value from brief
